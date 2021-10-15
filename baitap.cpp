ace27265ae989055678e5994ef7fe2e505b6880a
latrongthuong2k <74745602+latrongthuong2k@users.noreply.github.com>
2021-10-15 17:24:00 +0900

    Update baitap.cpp

--- baitap.cpp
+++ baitap.cpp
@@ -1,115 +1,59 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <cmath>
-using namespace std;
-/*
-1 /
-00101100 + 01010101 = 10000001
-10110000 + 00001111 = 10111111
-2/ 
-00101100 + 01010101 = 129
-10110000 + 00001111 = 191
-3/
-char : 1 bytes
-int : 2 また 4 bytes
-float ：4 bytes
-double ：8 bytes
 
-4/
-char * : 8 bytes
-int * : 8 bytes
-float *  : 8 bytes
-double * : 8 bytes
-5/ 
-// ② は実行されます。
 
-6/ 
+# include <iostream>
+class editor
+{
+    
+    public:
+        editor()
+        {
+            newArray = nullptr;
+
+        }
+        ~editor()
+        {
+            delete[] newArray;
+            newArray = nullptr;
+            printf("%p\n",newArray);
+        }
+        //
+    void Create(int numArr)
+    {
+        newArray = new int[numArr];
+        sizeofNumber = numArr;
+
+    }
+    int get(int getNumArr) 
+    {
+        if(sizeofNumber >= 0 && getNumArr < sizeofNumber)
+            return newArray[getNumArr] ;
+        
+    }
+    int set(int setNumArr) 
+    {
+        if(sizeofNumber >= 0 && setNumArr < sizeofNumber)
+        newArray[setNumArr] = setNumArr;
+       
+    }
+private:
+     int* newArray;      
+     int sizeofNumber;
+     
+};
 int main()
 {
-    int A, B;
-    printf("Get A , B \n");
-    scanf("%d\n",&A);
-    scanf("%d\n",&B);
-   if (A % 2 == 0)
-   {
-       printf(" A + B = %d ", A+B);
-   }else
-   {
-       printf(" A - B = %d ", A-B);
-   }
-return 0;
+    editor test;
+    test.Create(1000);
+    for (int i = 0; i < 1000; i++)
+        {
+            test.set(i);
+            
+        }
+    for (int i = 0; i < 1000; i++)
+        {
+            
+            printf("%d\n",  test.get(i));
+        }
+    
+
 }
-
-7/ 
-int main()
-{
-    int A, B;
-    bool end = false ;
-    printf("Get A , B \n");
-    do {
-        
-        scanf("%d\n",&A);
-        scanf("%d\n",&B);
-        
-       if (A % 2 == 0)
-       {
-           printf(" A + B = %d\n ", A+B);
-       }else
-       {
-           printf(" A - B = %d\n ", A-B);
-       }
-   }while ( A< 10 && B<10  );
-8/ 
-class Point
-{
-public:
-    Point() { Clear(); }
-
-    Clear()
-    {
-        x = 0;
-        y = 0;
-        z = 0;
-    }
-//public:　　＊こちら間違いた、消します 
-    int x, y, z;
-}
-
-{
-    Point point;
-    Point nextPoint;
-
-    nextPoint.x = 100;
-    nextPoint.y = 30;
-
-    while (1)
-    {
-        if (point.X < nextPoint.x)
-        {
-            point.x++;
-        }
-        else if (point.x > nextPoint.x)
-        {
-            point.x--;
-        }
-
-        if (point.y < nextPoint.y)
-        {
-            point.y++;
-        }
-        else if (point.y > nextPointy)
-        {
-            point.y--;
-        }
-        printf("pointX = %d\n", point.x);
-        printf("pointY = %d\n", point.y);
-
-        if (point.x == nextPoint.x &&
-            point.y == nextPoint.y)
-        {
-            break;
-        }
-    }
-}
-*/
