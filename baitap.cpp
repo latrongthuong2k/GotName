7ea2908a443c7306dccbd4912850e8e0e4c7ac32
latrongthuong2k <74745602+latrongthuong2k@users.noreply.github.com>
2021-10-16 14:10:18 +0900

    Update baitap.cpp

--- baitap.cpp
+++ baitap.cpp
@@ -1,45 +1,48 @@
-
-
 # include <iostream>
 class editor
 {
-    
+    private:
+     int* newArray;      
+     int sizeofNumber;
+
     public:
         editor()
         {
             newArray = nullptr;
-
         }
         ~editor()
         {
             delete[] newArray;
             newArray = nullptr;
-            printf("%p\n",newArray);
+            
         }
         //
     void Create(int numArr)
     {
         newArray = new int[numArr];
         sizeofNumber = numArr;
-
     }
+    
     int get(int getNumArr) 
     {
-        if(sizeofNumber >= 0 && getNumArr < sizeofNumber)
-            return newArray[getNumArr] ;
-        
+        if(sizeofNumber <= 0 || getNumArr > sizeofNumber)
+        {
+        printf("out of arr\n");
+        }
+            return newArray[getNumArr];   
     }
+    
     int set(int setNumArr) 
     {
-        if(sizeofNumber >= 0 && setNumArr < sizeofNumber)
-        newArray[setNumArr] = setNumArr;
+        if(sizeofNumber <= 0 || setNumArr > sizeofNumber)
+        {
+        printf("out of arr\n");
+        }
+        return newArray[setNumArr] = setNumArr;
        
-    }
-private:
-     int* newArray;      
-     int sizeofNumber;
-     
+    }   
 };
+
 int main()
 {
     editor test;
@@ -52,7 +55,7 @@ int main()
     for (int i = 0; i < 1000; i++)
         {
             
-            printf("%d\n",  test.get(i));
+            printf("num = %d\n", test.get(i));
         }
     
 
